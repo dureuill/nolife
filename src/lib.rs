4523e03109d44b043c1aecc45b139a4ae4660f38
Louis Dureuil <louis.dureuil@xinra.net>
2022-10-21 11:49:03 +0200

    Extract waker mod to file

--- src/lib.rs
+++ src/lib.rs
@@ -134,25 +134,7 @@ where
 
 /// From <https://blog.aloni.org/posts/a-stack-less-rust-coroutine-100-loc/>, originally from
 /// [genawaiter](https://lib.rs/crates/genawaiter).
-mod waker {
-    use std::task::{RawWaker, RawWakerVTable, Waker};
-
-    pub fn create() -> Waker {
-        // Safety: The waker points to a vtable with functions that do nothing. Doing
-        // nothing is memory-safe.
-        unsafe { Waker::from_raw(RAW_WAKER) }
-    }
-
-    const RAW_WAKER: RawWaker = RawWaker::new(std::ptr::null(), &VTABLE);
-    const VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, drop);
-
-    unsafe fn clone(_: *const ()) -> RawWaker {
-        RAW_WAKER
-    }
-    unsafe fn wake(_: *const ()) {}
-    unsafe fn wake_by_ref(_: *const ()) {}
-    unsafe fn drop(_: *const ()) {}
-}
+mod waker;
 
 impl<'a, 'b, T> Future for NolifeFuture<'a, 'b, T>
 where
---
+++ src/waker.rs
@@ -0,0 +1,17 @@
+use std::task::{RawWaker, RawWakerVTable, Waker};
+
+pub fn create() -> Waker {
+    // Safety: The waker points to a vtable with functions that do nothing. Doing
+    // nothing is memory-safe.
+    unsafe { Waker::from_raw(RAW_WAKER) }
+}
+
+const RAW_WAKER: RawWaker = RawWaker::new(std::ptr::null(), &VTABLE);
+const VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, drop);
+
+unsafe fn clone(_: *const ()) -> RawWaker {
+    RAW_WAKER
+}
+unsafe fn wake(_: *const ()) {}
+unsafe fn wake_by_ref(_: *const ()) {}
+unsafe fn drop(_: *const ()) {}
